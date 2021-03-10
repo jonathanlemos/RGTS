d69013af09afaa49627fbb1bcdc7038f50b2039f
Rômulo Martins <rm_romulomartins@hotmail.com>
2021-03-09 21:02:49 -0300

    add swagger

--- RGTS.API/Controllers/PermissaoController.cs
+++ RGTS.API/Controllers/PermissaoController.cs
@@ -1,8 +1,10 @@
 ﻿using Dominio.Entidades;
 using Dominio.Interfaces.Servicos;
 using Dominio.ValueType;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RGTS.API.Controllers
@@ -18,20 +20,20 @@ namespace RGTS.API.Controllers
             _permissaoServico = permissaoServico;
         }
 
-        [HttpGet]
-        public Permissao Get(int id)
-        {
-            return _permissaoServico.GetById(id);
-        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Permissao), StatusCodes.Status200OK)]
+        public ActionResult<Permissao> Get(int id) =>  _permissaoServico.GetById(id);
+
 
         [HttpGet]
-        public Permissao[] Get()
-        {
-            return _permissaoServico.GetAll().ToArray();
-        }
+        [ProducesResponseType(typeof(IEnumerable<Permissao>), StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Permissao>> Get() => Ok(_permissaoServico.GetAll());
+        
 
         [HttpPost]
-        public ActionResult<NotificacaoPost> Post(Permissao perfil)
+        [ProducesResponseType(typeof(NotificacaoPost), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<NotificacaoPost> Salvar(Permissao perfil)
         {
             try
             {
